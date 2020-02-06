f10d6617868dd086f6f89eedc69ad00f29a88a53
MobiComp <priyankasomani748@gmail.com>
2020-02-06 16:53:33 +0530

    Added : Number To Word Using Case

---
+++ Single_Digit_No.sh
@@ -0,0 +1,37 @@
+#! bin/bash
+
+read -p "Enter The Single Digit Number :" n
+
+case $n  in 1)
+
+	echo "one"
+	;;
+2) 
+	echo "Two"
+	;;
+3) 
+        echo "Three"
+        ;;
+4) 
+        echo "Four"
+        ;;
+5) 
+        echo "Five"
+        ;;
+6) 
+        echo "Six"
+        ;;
+7) 
+        echo "Seven"
+        ;;
+8) 
+        echo "Eight"
+        ;;
+9) 
+        echo "Nine"
+        ;;
+0) 
+        echo "Zero"
+        ;;
+
+esac
