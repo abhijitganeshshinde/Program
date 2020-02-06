c3aa4d8fc1f2eacdf7dbe010398a5f3e89bca011
MobiComp <priyankasomani748@gmail.com>
2020-02-06 15:24:06 +0530

    Refactor : minor change

--- Number.sh
+++ Number.sh
@@ -1,31 +1,24 @@
 #! bin/bash -x
 
 
-read -p "Enter The 1, 10 ,100 ,1000 ,10000 ,100000 : " n
+read -p "Enter The Value : " n
 
-if [[ $n == 1 ]];
-then
-        echo "One"
-
-elif [[ $n == 10 ]];
+if (( $n > 9 && $n < 99 ))
 then
         echo "Ten"
 
-elif [[ $n == 100 ]];
+elif (( $n > 99 && $n < 999 ))
 then
-        echo "Hundare"
+        echo "Hundared"
 
-elif (( $n == 1000 ))
+elif (( $n > 999 && $n < 9999 ))
 then
         echo "Thousand"
 
-elif (( $n == 10000 ))
+elif (( $n > 9999 && $n < 99999 ))
 then
         echo "Ten Thousand"
 
-elif (( $n == 100000 ))
-then
-        echo "1 Lac"
 else
-	echo "Error "
+	echo "Plz Don't enter single digit value "
 fi
