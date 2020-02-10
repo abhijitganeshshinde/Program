2f6680e9331067d74e555ff07c1bd91270829efd
MobiComp <priyankasomani748@gmail.com>
2020-02-10 10:44:56 +0530

    Added : check the no is prime then show that it palindrome is alos prime

---
+++ primepalindrome.sh
@@ -0,0 +1,43 @@
+#! bin/bash
+read -p "Enter The No" num1
+
+function myfunc() {
+
+num=$1
+for (( i=2; i<=num/2; i++ ))
+do 
+	if (( $num % $i == 0 ))
+	then
+	echo "Not Prime"
+	exit
+	fi
+done
+	echo "Prime"
+}
+
+function myfunc1(){
+
+num=$1
+sum=0;
+temp=$num
+rem=0;
+	while (( $num > 0 ))
+	do
+		rem=$(($num%10))
+		sum=$((($sum*10)+$rem))
+		num=$(($num/10))
+	done
+	if (( $temp == $sum ))
+	then
+	echo "Palindrome is also "
+	res=$( myfunc $num1 )
+	echo $res
+	else
+	echo "Not Palindrome"
+	fi
+	
+}
+res=$( myfunc $num1 )
+echo $res
+res=$( myfunc1 $num1 )
+echo $res
