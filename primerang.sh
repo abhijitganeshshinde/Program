f40f63e3d4c20f57be2ecb1837ca57c446f4feb2
abhijitganeshshinde <abhijitganeshshinde@gmail.com>
2020-02-10 12:42:46 +0530

    Added : Prime range using for loop

---
+++ primerang.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -x
+read -p "Enter the range " end
+
+for (( j=1; j<=$end; j++ ))
+do
+	prime=1
+
+	for (( i=2; i<=$j/2; i++ ))
+	do
+        	if (( $j % $i == 0 ))
+        	then
+                	prime=$(($prime-1))
+       		break;
+        	fi
+		done
+
+if (( $prime == 1 ))
+then
+echo "$j is Prime "
+fi
+done
